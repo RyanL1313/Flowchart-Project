e53d9eb579393cab2d7f89e9559f22cf293044d4
Ryan Lynch <ryn.lynch@gmail.com>
2020-02-17 20:32:46 -0600

    Add a main java file for our project.

---
+++ FlowchartMain/src/FlowchartMain.java
@@ -0,0 +1,5 @@
+public class FlowchartMain {
+    public static void main(String[] args) {
+        System.out.println("This is the main class for our whole project. Just testing things out.");
+    }
+}
