88e4646b993dbd25b9a22e3086af443d1745966c
Jossy <perezjossy00@gmail.com>
2020-03-20 16:44:10 -0500

    add_Degree_and_DegreeParser_classes

--- FlowchartMain/src/Degree.java
+++ FlowchartMain/src/Degree.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.*;
 
+/**
+ * A Degree class has a major, minor (if applicable), concentration (if applicable), and semesters.
+ */
 public class Degree {
+
+    //private ArrayList <Semester> semesterList;
+    private String major;
+    private String minor;
+    private String concentration;
+    private int MAX_SEMESTERS = 8;
+
+
+    public Degree() {
+    }
+
+    /**
+     * Getter for the degree major
+     *
+     * @return major
+     */
+    public String getMajor() {
+        return major;
+    }
+
+    /**
+     * Setter for the major
+     *
+     * @param major The Degree's major
+     */
+    public void setMajor(String major) {
+
+    }
+
+    /**
+     * Setter for the minor
+     *
+     * @param minor The Degree's minor
+     */
+    public void setMinor(String minor) {
+
+    }
+
+    /**
+     * Setter for the concentration
+     *
+     * @param concentration The Degree's concentration
+     */
+    public void setConcentration(String concentration) {
+
+    }
+
 }
--- FlowchartMain/src/DegreeParser.java
+++ FlowchartMain/src/DegreeParser.java
@@ -1,2 +1,76 @@
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * A DegreeParser class scans a file that has information of a degree and sends that information to Degree class.
+ */
 public class DegreeParser {
+
+    public static void main(String[] args) throws IOException {
+
+        String fileName = "FlowchartMain/test.html";
+        Document doc = Jsoup.parse(new File(fileName), "utf-8");
+        JSONObject jsonParentObject = new JSONObject();
+
+        /*
+        for(Element divTag: doc.select("div")){
+                for(Element table : doc.select("table")){
+                     for (Element row : table.select("tr")) {
+                        if (row.select("tr").is("tr")) {
+
+                            System.out.println(row.text());
+                        }
+                         }
+                }
+
+        }*/
+
+        doc.select("div#tabs").remove();
+        doc.select("div#textcontainer").remove();
+        doc.select("div#requirementstextcontainer").remove();
+        for(Element table: doc.select("table")){
+            for (Element row : table.select("tr")) {
+                if (row.select("tr").is("tr")){
+
+                    System.out.println(row.text());
+
+
+                }
+            }
+
+        }
+
+
+        //    Element divTag = doc.getElementById("pathwaystextcontainer");
+        //  Elements table = divTag.getElementsByTag("th");
+
+
+        //Element whatev = doc.get
+        //   System.out.println(divTag.text());
+        //  System.out.println(table.text());
+
+
+
+
+
+       /*Degree degree = new Degree();
+        String filename = degree.getMajor();
+        Scanner scanMajorFile = null;
+
+        try {
+            scanMajorFile = new Scanner(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+          */  //Do something here that let's us know this major doesn't exist yet
+    }
 }
+
+
+
+
+
+
